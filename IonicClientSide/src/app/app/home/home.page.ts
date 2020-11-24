fecc01ea10ca9d4900553349c44eb8d4e0bf1240
saralevovitz <68620461+saralevovitz@users.noreply.github.com>
2020-11-25 00:14:37 +0200

    Add files via upload

---
+++ IonicClientSide/src/app/app/home/home.page.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { RouteConfigLoadEnd, Router, RouterLink, RouterLinkActive } from '@angular/router';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  
+  constructor(private router: Router) {
+  }
+  
+  toSignUpPage(){  
+    this.router.navigate(['sign-up']);
+    }
+  
+    toLoginPage(){
+    this.router.navigate(['login']);
+    }
+  
+}
---
+++ IonicClientSide/src/app/app/pages/login/login.page.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  constructor(private router: Router) { }
+
+
+  //לקשר לסי שארפ
+  login(){
+  }
+
+  toHomePage(){
+    this.router.navigate(['home']);
+  }
+
+  toMyAccountPage(){
+    this.router.navigate(['my-account']);
+  }
+  
+  ngOnInit() {
+  }
+
+}
