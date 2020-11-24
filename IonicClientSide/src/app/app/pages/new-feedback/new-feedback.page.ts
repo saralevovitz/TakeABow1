c5ea09430581cba34828cc854dfaeeb9f8592541
saralevovitz <68620461+saralevovitz@users.noreply.github.com>
2020-11-25 00:18:13 +0200

    Add files via upload

---
+++ IonicClientSide/src/app/app/pages/new-feedback/new-feedback.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-new-feedback',
+  templateUrl: './new-feedback.page.html',
+  styleUrls: ['./new-feedback.page.scss'],
+})
+export class NewFeedbackPage implements OnInit {
+
+  constructor(private router: Router) { }
+
+  //לחבר לסי שארפ- הוספת פידבק
+  sendFeedback(){
+
+  }
+  
+  ngOnInit() {
+  }
+
+}
