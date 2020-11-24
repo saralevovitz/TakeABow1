ae14658c16b7504cee7fc229224f7a6db79d19d6
saralevovitz <68620461+saralevovitz@users.noreply.github.com>
2020-11-25 00:16:58 +0200

    Add files via upload

---
+++ IonicClientSide/src/app/app/pages/my-feedbacks/my-feedbacks.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-my-feedbacks',
+  templateUrl: './my-feedbacks.page.html',
+  styleUrls: ['./my-feedbacks.page.scss'],
+})
+export class MyFeedbacksPage implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+
+}
